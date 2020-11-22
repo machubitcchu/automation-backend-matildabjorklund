393576b6f4152f15ac0b7a83b29f877960662510
Matilda Björklund <matiildabjorklund@hotmail.com>
2020-11-22 11:37:19 +0100

    Added gitignore

--- backend-tests/cypress/integration/backend-auth.spec.js
+++ backend-tests/cypress/integration/backend-auth.spec.js
@@ -1,2 +1,24 @@
 /// <reference types="cypress" />
 
+import * as clientHelpers from '../helpers/clientHelpers'
+import * as billHelpers from '../helpers/billHelpers'
+
+describe('Test suite', function(){
+
+    it('TC01 - Create client', function(){
+
+    })
+
+    it('TC02 - Edit client', function(){
+        
+    })
+
+    it('TC03 - Create bill', function(){
+        
+    })
+
+    it('TC04 - Delete bill', function(){
+        
+    })
+    
+  })
